8e2c2d3fb3a3794f278a0837791c8303622dce8e
Nicolas Vaagen <nicolasvaagen@gmail.com>
2026-02-18 17:49:56 -0600

    added PlayerABC to player pkg

--- src/gambitpairing/models/player/__init__.py
+++ src/gambitpairing/models/player/__init__.py
@@ -1,13 +1,15 @@
-from gambitpairing.models.player.base_player import Player
-from gambitpairing.models.player.factory import (
+from .base_player import Player
+from .factory import (
     PlayerFactory,
     create_player,
     create_player_from_dict,
 )
-from gambitpairing.models.player.fide_player import FidePlayer
+from .fide_player import FidePlayer
+from .abc_player import PlayerABC
 
 __all__ = [
     "Player",
+    "PlayerABC",
     "FidePlayer",
     "PlayerFactory",
     "create_player",
