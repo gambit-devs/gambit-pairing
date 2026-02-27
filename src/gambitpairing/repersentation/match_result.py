fb255aacd405ed9416b9e811d501dec38077940e
Nicolas Vaagen <nicolasvaagen@gmail.com>
2026-02-27 09:52:20 -0600

    Implemented the MatchResult representation

--- src/gambitpairing/repersentation/match_result.py
+++ src/gambitpairing/repersentation/match_result.py
@@ -1,5 +1,6 @@
 """Data transformation/serialization/to-from dict for MatchResult."""
 
+
 # Gambit Pairing
 # Copyright (C) 2025  Gambit Pairing developers
 #
@@ -15,21 +16,77 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    def to_dict(self) -> Dict[str, Any]:
-        """Serialize match result to dictionary."""
-        return {
-            "white_id": self.white_id,
-            "black_id": self.black_id,
-            "white_score": self.white_score,
-        }
+def match_result_to_dict(match_result: MatchResult) -> Dict[str, Any]:
+    """Project a MatchResult into a dictionary representation.
 
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "MatchResult":
-        """Deserialize match result from dictionary."""
-        return cls(
-            white_id=data["white_id"],
-            black_id=data["black_id"],
-            white_score=data["white_score"],
-        )
+    This function converts a :class:`~gambitpairing.models.tournament.match_result.MatchResult`
+    aggregate into a plain dictionary composed only of Python primitives.
+    The resulting representation is suitable for storage, logging,
+    undo/redo snapshots, and export.
+
+    Parameters
+    ----------
+    match_result: MatchResult
+        MatchResult stored in the dict.
+
+    Returns
+    -------
+    dict
+        Dictionary representation of the MatchResult.
+
+    Notes
+    -----
+    This function performs no I/O and has no side effects. It is intended
+    to be a pure projection of domain state.
+
+    The representation is *lossless*: reconstructed via
+    :func:`match_result_from_dict` will be semantically equivalent.
+
+    See Also
+    --------
+    match_result_from_dict : Reconstruct a Tournament from its dictionary
+        representation.
+    """
+    return {
+        "white_id": match_result.white_id,
+        "black_id": match_result.black_id,
+        "white_score": match_result.white_score,
+    }
+
+
+def match_result_from_dict(data: Dict[str, Any]) -> MatchResult:
+    """Retrieve a MatchResult from the dictionary representation.
+
+    This function converts a  dictionary reperesentation into
+    :class:`~gambitpairing.models.tournament.match_result.MatchResult`
+
+    Parameters
+    ----------
+    dict
+        Dictionary representation of the MatchResult.
+
+    Returns
+    -------
+    match_result: MatchResult
+        MatchResult stored in the dict.
+
+    Notes
+    -----
+    This function performs no I/O and has no side effects. It is intended
+    to be a pure projection of domain state.
+
+    The representation is *lossless*: reconstructed via
+    :func:`match_result_from_dict` will be semantically equivalent.
+
+    See Also
+    --------
+    match_result_to_dict : Create a Tournament dictionary representation.
+    """
+    return cls(
+        white_id=data["white_id"],
+        black_id=data["black_id"],
+        white_score=data["white_score"],
+    )
+
 
 #  LocalWords:  MatchResult
